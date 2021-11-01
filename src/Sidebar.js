1ec655b3b0db5863fd6fb938bf87754e47e1d0b6
TheWolverine796 <pandeymrityunjay796@gmail.com>
2021-11-01 21:08:09 +0530

    hover

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>i'm the sidebar</h1>
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your Library" />
     </div>
   );
 }
