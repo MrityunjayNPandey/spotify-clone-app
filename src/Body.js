94a54a6c3cec673ae7c4562aaa500619fd5b34f1
TheWolverine796 <pandeymrityunjay796@gmail.com>
2021-11-03 14:42:06 +0530

    Added songrow

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./Body.css";
 import Header from "./Header";
 import { useDataLayerValue } from "./DataLayer";
+import {
+  Favorite,
+  More,
+  MoreHoriz,
+  PlayCircleFilled,
+} from "@mui/icons-material";
+import SongRow from "./SongRow";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -17,6 +24,18 @@ function Body({ spotify }) {
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilled className="body__shuffle" />
+          <Favorite fontSize="large" />
+          <MoreHoriz />
+        </div>
+
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
