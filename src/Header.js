e3790a05e282c5653d34cc8e660ce58edde18cd6
TheWolverine796 <pandeymrityunjay796@gmail.com>
2021-11-04 17:18:16 +0530

    adding search functionality

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import { Input } from "@mui/material";
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import { Avatar } from "@mui/material";
 import { useDataLayerValue } from "./DataLayer";
+import { Container, Form } from "react-bootstrap";
+import SpotifyWebApi from "spotify-web-api-node";
 function Header() {
   const [{ user }, dispatch] = useDataLayerValue();
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+  const [lyrics, setLyrics] = useState("");
+
   return (
     <div className="header">
       <div className="header__left">
         <SearchIcon />
-        <input
+        <Form.Control
+          type="search"
           placeholder="Search for Artists, Songs, or Podcasts "
-          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
 
